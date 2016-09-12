fe694be6d99c476fb33fa26770731f31086fb9aa
DYTEC <netdevag@gmail.com>
2016-09-12 13:42:56 +0200

    Initial commit

---
+++ js/todo.js
@@ -0,0 +1,71 @@
+function get_todos() {
+  var todos = new Array;
+  var todos_str = localStorage.getItem('todo');
+  if (todos_str != null) {
+    todos = JSON.parse(todos_str);
+  }
+  return todos;
+}
+
+function add() {
+  var task = document.getElementById('task').value;
+
+  var todos = get_todos();
+  todos.push(task);
+  localStorage.setItem('todo', JSON.stringify(todos));
+
+  document.getElementById('task').value = null;
+  document.getElementById('task').focus();
+
+  show();
+
+  return false;
+}
+
+function show() {
+  var todos = get_todos();
+
+  var html = '<ul>';
+  for (var i=0; i<todos.length; i++) {
+    html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
+  };
+  html += '</ul>';
+
+  document.getElementById('todos').innerHTML = html;
+
+  var buttons = document.getElementsByClassName('remove');
+  for (var i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', remove);
+  };
+}
+
+function remove() {
+  var id = this.getAttribute('id');
+  var todos = get_todos();
+  todos.splice(id, 1);
+  localStorage.setItem('todo', JSON.stringify(todos));
+
+  show();
+
+  return false;
+}
+
+function clear() {
+  localStorage.clear();
+
+  show();
+
+  document.getElementById('task').focus();
+
+  return false;
+}
+
+function checkEnter(e) {
+  if(e && e.keyCode == 13) {
+    add();
+  }
+}
+
+document.getElementById('add').addEventListener('click', add);
+document.getElementById('clear').addEventListener('click', clear);
+show();
